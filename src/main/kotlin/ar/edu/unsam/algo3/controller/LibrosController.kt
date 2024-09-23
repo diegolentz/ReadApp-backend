0963aa30f68ea6d439944ab98236ad69e71271f5
ValentinPg <105302328+ValentinPg@users.noreply.github.com>
2024-09-22 22:22:18 -0300

    Cambio el Libro DTO para que sea parte del controller y no interfiera con el service

--- src/main/kotlin/ar/edu/unsam/algo3/controller/LibrosController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/LibrosController.kt
@@ -1,14 +1,12 @@
 package ar.edu.unsam.algo3.controller
 
+import LibroBuilder
+import ar.edu.unsam.algo2.readapp.libro.Autor
+import ar.edu.unsam.algo2.readapp.libro.Lenguaje
 import ar.edu.unsam.algo2.readapp.libro.Libro
-import ar.edu.unsam.algo3.services.LibroDTO
+import ar.edu.unsam.algo3.services.ServiceAutor
 import ar.edu.unsam.algo3.services.ServiceLibros
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class LibrosController(val serviceLibros: ServiceLibros) {
@@ -20,7 +18,7 @@ class LibrosController(val serviceLibros: ServiceLibros) {
     fun libroPorId(@PathVariable id: Int) = serviceLibros.getById(id)
 
     @PostMapping("/libros")
-    fun crearLibro(@RequestBody libroBody: LibroDTO): Libro = serviceLibros.nuevoLibro(libroBody)
+    fun crearLibro(@RequestBody libroBody: LibroDTO): Libro = serviceLibros.nuevoLibro(libroBody.convertir())
 
     @PutMapping("/libros")
     fun actualizarLibro(@RequestBody nuevoLibro: Libro): Libro {
@@ -28,4 +26,35 @@ class LibrosController(val serviceLibros: ServiceLibros) {
         return serviceLibros.getById(nuevoLibro.id)
     }
 
+}
+
+class LibroDTO(
+    val id_autor: Int = 0,
+    var cantidadPalabras: Int = 1,
+    var cantidadPaginas: Int = 1,
+    var ediciones: Int = 1,
+    var ventasSemanales: Int = 0,
+    var traducciones: MutableSet<Lenguaje> = mutableSetOf(),
+    var titulo: String = ""
+) {
+    var autor: Autor = buscarAutor()
+
+    fun buscarAutor(): Autor {
+        val autorExistente = ServiceAutor.get().find { it.id == id_autor }
+        if (autorExistente != null) {
+            return autorExistente
+        }
+//        Acá debería devolver una excepción avisandoq ue el autor no existe, que hay que crearlo primero o revisar que su ide este bien
+        return Autor()
+    }
+
+    fun convertir(): Libro =
+        LibroBuilder().autor(autor)
+            .cantidadPalabras(cantidadPalabras)
+            .cantidadPaginas(cantidadPaginas)
+            .ediciones(ediciones)
+            .ventasSemanales(ventasSemanales)
+            .traducciones(traducciones)
+            .titulo(titulo)
+            .build()
 }
--- src/main/kotlin/ar/edu/unsam/algo3/services/services.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/services/services.kt
@@ -1,8 +1,6 @@
 package ar.edu.unsam.algo3.services
 
-import LibroBuilder
 import ar.edu.unsam.algo2.readapp.libro.Autor
-import ar.edu.unsam.algo2.readapp.libro.Lenguaje
 import ar.edu.unsam.algo2.readapp.libro.Libro
 import ar.edu.unsam.algo2.readapp.repositorios.Repositorio
 import org.springframework.stereotype.Service
@@ -14,10 +12,9 @@ object ServiceLibros {
 
     fun get(): List<Libro> = repo.getAll().toList()
 
-    fun nuevoLibro(libro: LibroDTO): Libro {
-        val libroNuevo = libro.convertir()
-        repo.create(libroNuevo)
-        return libroNuevo
+    fun nuevoLibro(libro: Libro): Libro {
+        repo.create(libro)
+        return getById(libro.id)
     }
 
     fun getById(libroId: Int): Libro = repo.getByID(libroId)
@@ -33,33 +30,3 @@ object ServiceAutor {
 
 }
 
-class LibroDTO(
-    val id_autor: Int = 0,
-    var cantidadPalabras: Int = 1,
-    var cantidadPaginas: Int = 1,
-    var ediciones: Int = 1,
-    var ventasSemanales: Int = 0,
-    var traducciones: MutableSet<Lenguaje> = mutableSetOf(),
-    var titulo: String = ""
-) {
-    var autor: Autor = buscarAutor()
-
-    fun buscarAutor(): Autor {
-        val autorExistente = ServiceAutor.get().find { it.id == id_autor }
-        if (autorExistente != null) {
-            return autorExistente
-        }
-//        Acá debería devolver una excepción avisandoq ue el autor no existe, que hay que crearlo primero o revisar que su ide este bien
-        return Autor()
-    }
-
-    fun convertir(): Libro =
-        LibroBuilder().autor(autor)
-            .cantidadPalabras(cantidadPalabras)
-            .cantidadPaginas(cantidadPaginas)
-            .ediciones(ediciones)
-            .ventasSemanales(ventasSemanales)
-            .traducciones(traducciones)
-            .titulo(titulo)
-            .build()
-}
